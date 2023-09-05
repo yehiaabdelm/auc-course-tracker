630fbdf06bf5842af7f537927847cce7a5fa2d05
Yehia <yehiaabdelm@gmail.com>
2023-09-05 15:02:03 +0300

    Update models.js
    Track emails sent

--- src/lib/server/models.js
+++ src/lib/server/models.js
@@ -9,7 +9,13 @@ const courseSchema = new mongoose.Schema({
     emails: [String],
 }, { timestamps: true });
 
+const emailSchema = new mongoose.Schema({
+    recipients: [String],
+    subject: String,
+    body: String
+},{timestamps: true})
 
 const Course = mongoose.model('course', courseSchema);
+const Email = mongoose.model('email',emailSchema)
 
-export { Course }
+export { Course, Email }
